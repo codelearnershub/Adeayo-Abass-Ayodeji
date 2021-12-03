db3ebb5259e4496732cb46828ca5c727c0b80257
DefinedCodes <greatmoh007@gmail.com>
2021-12-03 15:59:04 +0100

    Code Challenge solution(s)

---
+++ CodeLearnersHub/Question1/Question1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] arr = new long [5]{3,9,5,7,1};
+            
+            minMaxSum(arr);
+        }
+
+        static void minMaxSum(long[] numbers)
+        {
+            long sumMax = 0;
+            long sumMin = 0;
+            
+            Array.Sort(numbers);
+            sumMax += numbers[1] + numbers[2] + numbers[3] + numbers[4];
+            sumMin += numbers[0]+numbers[1]+numbers[2]+numbers[3];
+            Console.Write($"The min values of the positive arrays are min {sumMin}, max {sumMax}");
+        }
+    }
+}
---
+++ CodeLearnersHub/Question3/Question3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = new[] {1,2,3,3,2};
+            
+            
+            string[] tokens_n = Console.ReadLine().Split('j');
+            int n = Convert.ToInt32(tokens_n[0]);
+            int k = Convert.ToInt32(tokens_n[1]);
+            string[] height_temp = Console.ReadLine().Split(' ');
+            height = Array.ConvertAll(height_temp, Int32.Parse);
+
+
+            var max = height.Max();
+            var diff = Math.Max(max - k, 0);
+            Console.WriteLine(diff);
+        }
+    }
+}
