074cac2f30d76493768daaeb58bf7de87d4b2df4
Abass Adebayo A <greatmoh007@gmail.com>
2022-01-07 12:39:01 +0100

    Code Challenge(Question2)

--- CodeChallenge24thDec21/Question2/Question2_LongestPalindromicSubstring/Program.cs
+++ CodeChallenge24thDec21/Question2/Question2_LongestPalindromicSubstring/Program.cs
@@ -24,14 +24,14 @@ namespace Question2_LongestPalindromicSubstring
             {
                 for (int j = i; j < palString.Length; j++)
                 {
-                    int check = 1;
+                    bool check = true;
 
                     for (int k = 0; k < (j - i + 1) / 2; k++)
                         if (palString[i + k] != palString[j - k])
-                            check = 0;
+                            check = false;
 
 
-                    if (check != 0 && (j - i + 1) > maxLength)
+                    if (check != false && (j - i + 1) > maxLength)
                     {
                         initialize = i;
                         maxLength = j - i + 1;
---
+++ Question2_9thJan2022/Question2_9thJan2022/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Question2_9thJan2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = {1, 1, 1, 2, 2, 3};
+
+            int expectedNumbers = nums.Length / nums[0];
+            
+            RemoveDuplicates(nums, expectedNumbers);
+            
+        }
+        
+        
+        public static void RemoveDuplicates(int[] arrNumbers, int expNumbers)
+        {
+            int counter = 0;
+            
+            
+
+            for (int i = 0; i < expNumbers; i++)
+            {
+                if (i < expNumbers - 2 &&
+                    (arrNumbers[i] == arrNumbers[i + 1] && (arrNumbers[i] == arrNumbers[i + 2])))
+                {
+                    continue;
+                }
+                else
+                {
+                    arrNumbers[counter] = arrNumbers[i];
+                    counter++;
+                }
+                          
+
+            }
+            Console.Write($"Output: { counter}, nums = ");
+            
+            for (int j = 0; j< counter; j++)
+            {
+                Console.Write(arrNumbers[j]);
+                if (j!=counter-1)
+                {
+                    Console.Write(",");
+                }
+            }
+            
+            
+        }
+    }
+}
