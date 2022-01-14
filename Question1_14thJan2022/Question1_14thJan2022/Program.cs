39b4bbac850aa2bf9dc93e283a170719a8663767
Abass Adebayo A <greatmoh007@gmail.com>
2022-01-14 12:29:12 +0100

    Code Challenge

---
+++ Question1_14thJan2022/Question1_14thJan2022/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Question1_14thJan2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arr = {2, 10};
+
+            Console.WriteLine($"Input: [{string.Join(',' ,Arr)}]");
+           PrintTheLargestNum(Arr);
+        }
+
+
+        public static void PrintTheLargestNum(int[] arrNumbers)
+        {
+            int max = 0;
+            int IndexOfNum_WithMaxLeftMostDigit = 0;
+
+            for (int i = 0; i < arrNumbers.Length; i++)
+            {
+                int num = arrNumbers[i];
+
+                do
+                {
+                    int rem = num % 10;
+                    num = num / 10;
+
+                    if (num == 0)
+                    {
+                        if (max < rem)
+                        {
+                            max = rem;
+                            IndexOfNum_WithMaxLeftMostDigit = i;
+                        }
+                    }
+
+                } while (num > 0);
+
+            }
+            Console.Write("Output: "  );
+            for (int x = IndexOfNum_WithMaxLeftMostDigit; x < arrNumbers.Length; x++)
+            {
+                Console.Write(arrNumbers[x]);
+            }
+
+            for (int y = 0; y < IndexOfNum_WithMaxLeftMostDigit; y++)
+            {
+                Console.Write(arrNumbers[y]);
+            }
+        }
+    }
+    
+}
---
+++ Question2_14thJan2022/Question2_14thJan2022/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Question3_14thJan2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(IsValid(input));
+           
+        }
+        
+        public static bool IsValid(string input)
+        {
+            int count = 0;
+            if (input.Length%2 == 0) return true;
+            
+
+            if (input.Length % 2 != 0) return false;
+                
+
+
+            for(int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(' || input[i] == ')' || input[i] == '*')
+                {
+                    if (count == 0)
+                        return false;
+                    count--;
+                }
+                else
+                    count++;
+            }
+
+            return false;
+        }
+    }
+}
